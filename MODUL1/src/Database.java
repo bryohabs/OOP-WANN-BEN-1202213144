f75ad660d5c3d803b86b9134e36d99fa10cce0ca
bryohabs <bryohabs@student.telkomuniversity.ac.id>
2022-12-04 20:59:45 +0700

    TPMODUL2_BEN

---
+++ MODUL1/src/Database.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    String Nama;
+    Integer Handphone;
+    Integer price;
+    System.out.println("Selamat datang di restoran EAD");
+    System.out.println("Silahkan registrasi terlebih dahulu");
+    System.out.println("==============================");
+    // TODO Create Method to Insert Menu to Database
+    Scanner menu = new Scanner(System.in);
+    String pesan = menu.nextLine();
+    // TODO Create Method to Show Menu from Database
+
+    // TODO Create Method to Search Menu from Database
+
+
+}
---
+++ MODUL1/src/User.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    String nama;
+    Integer handphone;
+    Scanner Name = new Scanner(System.in);
+     String nama = Name.nextLine();
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
