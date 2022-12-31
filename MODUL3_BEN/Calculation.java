292184954305998af40e0e65f3ba3bb96eccf10c
bryohabs <115805309+bryohabs@users.noreply.github.com>
2022-12-31 07:36:24 +0700

    Add files via upload

---
+++ MODUL3_BEN/Calculation.java
@@ -0,0 +1,36 @@
+package MODUL3_BEN;
+
+public class Calculation implements Runnable {
+    Double radius, site, area;
+    private static double phi = 3.14;
+}
+
+public Main
+
+public void setSquare(){
+    double side;
+    this.Square = side*side;
+}
+
+public String getSquareString(){
+    return this.Square;
+}
+
+public void setCircle(){
+    double phi;
+    this.Circle = phi*(radius*radius);
+}
+
+public String getCircle(){
+    return this.Circle;
+}
+
+public void setCircle(){
+    double phi;
+    this.Circle = phi*(radius*radius);
+}
+
+public String getCircle(){
+    return this.Circle;
+}
+
---
+++ MODUL3_BEN/Main.java
@@ -0,0 +1,14 @@
+package MODUL3_BEN;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+   private double repeat = true;
+}
+
+public static void main(String[] args) {
+    // deklarasi variabel
+    while (true):
+        try :
+            Integer Circle : 
